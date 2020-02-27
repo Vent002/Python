6aff9081815be56881db08c71913982f68087b7c
unknown <gxminglee@qq.com>
2020-02-27 19:22:03 +0800

    day02

--- code/day02/百分制等级判断.py
+++ code/day02/百分制等级判断.py
@@ -1,17 +1,18 @@
 '''
 @Date: 2020-02-27 19:01:39
 @LastEditors: gxm
-@LastEditTime: 2020-02-27 19:05:43
+@LastEditTime: 2020-02-27 19:19:17
 @FilePath: \Python workplace\code\day02\百分制等级判断.py
 '''
 score = float(input('输入成绩：'))
 if score <= 100 and score >= 90:
-  print('A')
+  grade = 'A'
 elif score < 90 and score >= 80:
-  print('B')
+  grade = 'B'
 elif score < 80 and score >= 60:
-  print('C')
+  grade = 'C'
 elif score >= 0 and score < 60:
-  print('D')
+  grade = 'D'
 else:
   print('输入成绩无效')
+print('对应等级：',grade)
