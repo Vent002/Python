3b59f16866e610df987df8a62030f8db156ac814
gxming <gxminglee@qq.com>
2020-02-28 14:26:27 +0800

    dya03-3

--- code/day03/三角形图案.py
+++ code/day03/三角形图案.py
@@ -1,12 +1,27 @@
 '''
 @Date: 2020-02-27 20:58:22
 @LastEditors: gxm
-@LastEditTime: 2020-02-27 21:00:47
+@LastEditTime: 2020-02-28 14:25:12
 @FilePath: \Python workplace\code\day03\三角形图案.py
 '''
 import math
 row = int(input('行数：'))
 for i in range(0,row):
   for j in range(i+1):
+    print('*',end='')
+  print()
+
+for i in range(row):
+  for j in range(row):
+    if j < row - i - 1:
+      print(' ',end='')
+    else:
+      print('*',end='')
+  print()
+
+for i in range(row):
+  for j in range(row - i - 1):
+    print(' ',end='')
+  for k in range(2 * i + 1):
     print('*',end='')
   print()
